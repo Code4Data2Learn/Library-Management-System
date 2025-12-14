a1cbc8184ed35dfe51395bf9f04f7ea9f39e81c6
Code4Data2Learn <aldous275.learn@outlook.com>
2025-12-14 21:29:33 +0530

    Project Complete

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const usersRouter = require('./routes/user');
+const booksRouter = require('./routes/books');
 const express = require('express');
 const app = express();
 const PORT = 8081;
@@ -10,11 +12,8 @@ app.get("/",(req,res)=>{
     });
 });
 
-// app.all('*',(req,res)=>{
-//     res.status(500).json({
-//         message: "Not built yet!"
-//     })
-// });
+app.use('/users',usersRouter);
+app.use('/books',booksRouter);
 
 app.listen(PORT,()=>{
     console.log(`Server is up and running on http://localhost:${PORT}`);
